85cb440d5277118852b56bab69f33ed04cce6d76
kummancoder <kummandhakad@gmail.com>
2024-12-31 21:33:49 +0530

    utility classes and functions created

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-dotenv.config(
-    {
-        path: './.env'
-    }
-)
+import { app } from "./app.js";
+dotenv.config({
+  path: "./.env",
+});
 connectDB()
+  .then(() => {
+    app.on("error", () => {
+      console.log("Not Able to connect Express server");
+      throw error;
+    });
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`server is running at ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection Failed: " + error);
+  });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 // import dotenv from 'dotenv'
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
