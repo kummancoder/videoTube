d78b889800547d412e8826ec2feea81a427c8430
kummancoder <kummandhakad@gmail.com>
2025-01-04 14:19:54 +0530

    Implement refresh token functionality and enhance user authentication routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,9 +3,12 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
 const router = Router();
 
 router.route("/register").post(
@@ -25,4 +28,6 @@ router.route("/login").post(loginUser);
 
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken)
+
 export default router;
