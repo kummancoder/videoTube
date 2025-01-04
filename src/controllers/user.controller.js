fabd67ec838b4f0a6f8d95418e08775095a0de84
kummancoder <kummandhakad@gmail.com>
2025-01-04 14:53:01 +0530

    Add getCurrentUser function to user controller to retrieve current user details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -247,10 +247,20 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "User fetched successfully"
+    ))
+})
 export {
   registerUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
+  getCurrentUser
 };
